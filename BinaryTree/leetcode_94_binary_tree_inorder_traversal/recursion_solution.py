6cf38f706493a0e4c1cbba6127cb699e3fd39253
ytzhao <144186671+ytzhao2023@users.noreply.github.com>
2024-02-07 23:10:12 -0800

    delete .DS_Store file

--- BinaryTree/leetcode_94_binary_tree_inorder_traversal/recursion_solution.py
+++ BinaryTree/leetcode_94_binary_tree_inorder_traversal/recursion_solution.py
@@ -1,9 +1,9 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution(object):
     def inorderTraversal(self, root):
         """
@@ -18,3 +18,8 @@ class Solution(object):
         right = self.inorderTraversal(root.right)
 
         return left + [root.val] + right
+    
+solution = Solution()
+root = TreeNode(1, None, TreeNode(2, TreeNode(3), None))
+result = solution.inorderTraversal(root)
+print(result)
